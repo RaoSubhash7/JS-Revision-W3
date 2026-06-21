66ada85a63d62947f02e97c36e08e6df8237fe08
RaoSubhash7 <subhashkumar2005deoghar@gmail.com>
2026-06-21 19:54:52 +0530

    first commit

---
+++ JS-Intros/intro.js
@@ -0,0 +1,6 @@
+const intro = document.querySelector(".intro")
+const button = document.querySelector("button")
+
+button.addEventListener("click", ( ) => {
+intro.innerText = "Hello Subhash Bro you are clciked the button and lets Start the understanding jS ";
+});
