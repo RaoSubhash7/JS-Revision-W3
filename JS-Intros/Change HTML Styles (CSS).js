076c7b02e8247231c6c94f90eb29617b057def1a
RaoSubhash7 <subhashkumar2005deoghar@gmail.com>
2026-06-24 18:32:46 +0530

    Show HTML Elements , Hide html content Change HTML Styles (CSS)

---
+++ JS-Intros/Change HTML Styles (CSS).js
@@ -0,0 +1,6 @@
+const intro = document.querySelector(".intro")
+const button = document.querySelector("button")
+
+button.addEventListener("click" , () => {
+   intro.style.fontSize = "35px"
+})
---
+++ JS-Intros/Show HTML Elements.js
@@ -0,0 +1,6 @@
+const p = document.querySelector(".p")
+const button = document.querySelector("button")
+
+button.addEventListener("click" , ()=>{
+    p.style.display="block"
+})
